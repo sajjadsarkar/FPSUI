ec889275482f0397a746b09c811dd414ed8d2d1d
Sajjad Hossain <ms.sazzad.0000@gmail.com>
2024-12-29 15:31:13 +0600

    save

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -7,6 +7,10 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject panel1;
     [SerializeField] private GameObject panel2;
     [SerializeField] private GameObject panel3;
+    [SerializeField] private GameObject panel4;
+    [SerializeField] private GameObject panel5;
+    [SerializeField] private GameObject panel6;
+    [SerializeField] private GameObject panel7;
 
     [SerializeField] private GameObject BottomPanel;
     [SerializeField] private float transitionDuration = 0.5f;
@@ -16,20 +20,37 @@ public class MenuManager : MonoBehaviour
     private RectTransform panel1Rect;
     private RectTransform panel2Rect;
     private RectTransform panel3Rect;
+    private RectTransform panel4Rect;
+    private RectTransform panel5Rect;
+    private RectTransform panel6Rect;
+    private RectTransform panel7Rect;
 
-
-    private float slideDistance = 1000f; // Reduced slide distance
+    private float slideDistance = 1000f;
 
     [SerializeField] private List<GameObject> particleEffects;
-
     public GameObject bottompanel2;
+
     void Start()
     {
         Application.targetFrameRate = Screen.currentResolution.refreshRate;
         panel1Rect = panel1.GetComponent<RectTransform>();
         panel2Rect = panel2.GetComponent<RectTransform>();
         panel3Rect = panel3.GetComponent<RectTransform>();
+        panel4Rect = panel4.GetComponent<RectTransform>();
+        panel5Rect = panel5.GetComponent<RectTransform>();
+        panel6Rect = panel6.GetComponent<RectTransform>();
+        panel7Rect = panel7.GetComponent<RectTransform>();
         ShowPanel1();
+    }
+
+    private void HideAllPanels()
+    {
+        foreach (var particle in particleEffects)
+        {
+            particle.SetActive(false);
+        }
+        BottomPanel.SetActive(false);
+        bottompanel2.SetActive(false);
     }
 
     public void ShowPanel1()
@@ -79,54 +100,121 @@ public class MenuManager : MonoBehaviour
 
     public void ShowPanel3()
     {
-        bottompanel2.SetActive(false);
-        foreach (var particle in particleEffects)
-        {
-            particle.SetActive(false);
-        }
+        HideAllPanels();
         panel3.SetActive(true);
-        BottomPanel.SetActive(false);
         panel3Rect.anchoredPosition = new Vector2(slideDistance, 0);
         panel3Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
-        panel2Rect.DOAnchorPos(new Vector2(-slideDistance, 0), transitionDuration).SetEase(easeType)
-            .OnComplete(() => panel2.SetActive(false));
+
+        if (currentPanelIndex == 2)
+        {
+            panel2Rect.DOAnchorPos(new Vector2(-slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel2.SetActive(false));
+        }
+        else if (currentPanelIndex == 4)
+        {
+            panel4Rect.DOAnchorPos(new Vector2(slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel4.SetActive(false));
+        }
         currentPanelIndex = 3;
+    }
+
+    public void ShowPanel4()
+    {
+        HideAllPanels();
+        panel4.SetActive(true);
+        panel4Rect.anchoredPosition = new Vector2(slideDistance, 0);
+        panel4Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
+
+        if (currentPanelIndex == 3)
+        {
+            panel3Rect.DOAnchorPos(new Vector2(-slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel3.SetActive(false));
+        }
+        else if (currentPanelIndex == 5)
+        {
+            panel5Rect.DOAnchorPos(new Vector2(slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel5.SetActive(false));
+        }
+        currentPanelIndex = 4;
+    }
+
+    public void ShowPanel5()
+    {
+        HideAllPanels();
+        panel5.SetActive(true);
+        panel5Rect.anchoredPosition = new Vector2(slideDistance, 0);
+        panel5Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
+
+        if (currentPanelIndex == 4)
+        {
+            panel4Rect.DOAnchorPos(new Vector2(-slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel4.SetActive(false));
+        }
+        else if (currentPanelIndex == 6)
+        {
+            panel6Rect.DOAnchorPos(new Vector2(slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel6.SetActive(false));
+        }
+        currentPanelIndex = 5;
+    }
+
+    public void ShowPanel6()
+    {
+        HideAllPanels();
+        panel6.SetActive(true);
+        panel6Rect.anchoredPosition = new Vector2(slideDistance, 0);
+        panel6Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
+
+        if (currentPanelIndex == 5)
+        {
+            panel5Rect.DOAnchorPos(new Vector2(-slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel5.SetActive(false));
+        }
+        else if (currentPanelIndex == 7)
+        {
+            panel7Rect.DOAnchorPos(new Vector2(slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel7.SetActive(false));
+        }
+        currentPanelIndex = 6;
+    }
+
+    public void ShowPanel7()
+    {
+        HideAllPanels();
+        panel7.SetActive(true);
+        panel7Rect.anchoredPosition = new Vector2(slideDistance, 0);
+        panel7Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
+
+        if (currentPanelIndex == 6)
+        {
+            panel6Rect.DOAnchorPos(new Vector2(-slideDistance, 0), transitionDuration).SetEase(easeType)
+                .OnComplete(() => panel6.SetActive(false));
+        }
+        currentPanelIndex = 7;
     }
 
     public void GoBack()
     {
         switch (currentPanelIndex)
         {
-            case 3:
-                bottompanel2.SetActive(true);
-                foreach (var particle in particleEffects)
-                {
-                    particle.SetActive(false);
-                }
-                panel2.SetActive(true);
-                panel2Rect.anchoredPosition = new Vector2(slideDistance, 0);
-                panel2Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
-                panel3Rect.DOAnchorPos(new Vector2(-slideDistance, 0), 0.4f).SetEase(easeType)
-                    .OnComplete(() => panel3.SetActive(false));
-                currentPanelIndex = 2;
+            case 7:
+                ShowPanel6();
                 break;
-
+            case 6:
+                ShowPanel5();
+                break;
+            case 5:
+                ShowPanel4();
+                break;
+            case 4:
+                ShowPanel3();
+                break;
+            case 3:
+                ShowPanel2();
+                break;
             case 2:
-                panel1.SetActive(true);
-                bottompanel2.SetActive(false);
-                foreach (var particle in particleEffects)
-                {
-                    particle.SetActive(true);
-                }
-                panel1Rect.anchoredPosition = new Vector2(slideDistance, 0);
-                panel1Rect.DOAnchorPos(Vector2.zero, transitionDuration).SetEase(easeType);
-                panel2Rect.DOAnchorPos(new Vector2(-slideDistance, 0), 0.4f).SetEase(easeType)
-                    .OnComplete(() => panel2.SetActive(false));
-                currentPanelIndex = 1;
-                BottomPanel.SetActive(true);
-
+                ShowPanel1();
                 break;
         }
     }
-
 }
