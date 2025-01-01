044633edc142d0f0b635fee917137af4b68b6c5b
Sajjad Hossain <ms.sazzad.0000@gmail.com>
2025-01-01 21:34:22 +0600

    randomize shop

--- Assets/shop.cs
+++ Assets/shop.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Shop : MonoBehaviour
 {
@@ -7,12 +8,16 @@ public class Shop : MonoBehaviour
     [SerializeField] private Button[] shopButtons;
     [SerializeField] private Sprite selectedSprite;
     [SerializeField] private Sprite unselectedSprite;
+    [SerializeField] private Image[] itemImages;
+    [SerializeField] private Sprite[] itemSprites;
 
     private int currentSelectedIndex = -1;
+    private Dictionary<int, int[]> buttonItemMapping = new Dictionary<int, int[]>();
 
     void Start()
     {
-        // Initialize shop items with 0 opacity
+        InitializeButtonItems();
+
         for (int i = 0; i < shopImages.Length; i++)
         {
             Color imageColor = shopImages[i].color;
@@ -22,23 +27,69 @@ public class Shop : MonoBehaviour
             shopButtons[i].onClick.AddListener(() => SelectShopItem(index));
         }
 
-        // Default selection
         SelectShopItem(0);
+    }
+
+    private void InitializeButtonItems()
+    {
+        List<int> availableIndices = new List<int>();
+        for (int i = 0; i < itemSprites.Length; i++)
+        {
+            availableIndices.Add(i);
+        }
+
+        // Assign random items to each button
+        for (int buttonIndex = 0; buttonIndex < shopButtons.Length; buttonIndex++)
+        {
+            int[] itemIndices = new int[itemImages.Length];
+
+            // Get random items for this button
+            for (int itemIndex = 0; itemIndex < itemImages.Length; itemIndex++)
+            {
+                if (availableIndices.Count == 0)
+                {
+                    // Refill available indices if exhausted
+                    for (int i = 0; i < itemSprites.Length; i++)
+                    {
+                        availableIndices.Add(i);
+                    }
+                }
+
+                int randomIndex = Random.Range(0, availableIndices.Count);
+                itemIndices[itemIndex] = availableIndices[randomIndex];
+                availableIndices.RemoveAt(randomIndex);
+            }
+
+            buttonItemMapping[buttonIndex] = itemIndices;
+        }
     }
 
     public void SelectShopItem(int index)
     {
+        // Reset previous selection
         if (currentSelectedIndex >= 0)
         {
             Color imageColor = shopImages[currentSelectedIndex].color;
             imageColor.a = 0f;
             shopImages[currentSelectedIndex].color = imageColor;
+            shopImages[currentSelectedIndex].sprite = unselectedSprite;
         }
 
+        // Set new selection
         currentSelectedIndex = index;
         Color newImageColor = shopImages[currentSelectedIndex].color;
         newImageColor.a = 1f;
         shopImages[currentSelectedIndex].color = newImageColor;
         shopImages[currentSelectedIndex].sprite = selectedSprite;
+
+        // Show saved items for this button
+        if (buttonItemMapping.ContainsKey(index))
+        {
+            int[] itemIndices = buttonItemMapping[index];
+            for (int i = 0; i < itemImages.Length; i++)
+            {
+                itemImages[i].sprite = itemSprites[itemIndices[i]];
+            }
+        }
     }
 }
